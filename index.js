c85d53399824735861e0196800e8f3b140899386
lidya <lidya. abraham@student.moringaschool.com>
2022-08-21 21:30:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400) {
+    return 'This one is on me!';
+  }else if(feet > 2000 &&  feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if(feet > 2500) {
+    return 'No can do.';
+  }
+  
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
+  // Write your code here!function ternaryCheckCity(city){
   // Write your code here!
+  if(city==='NYC') {
+    return 'Ok, sounds good.';
+  }else(city==='Pittsburgh'); {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+   
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+    return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    case "thanks for everything" :
+      return "Bye." 
+
+  
+  }
+
 }
